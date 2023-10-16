9c6b031b216ecc61c7cb2841148d41f8668cf8d9
KoheiKusuyama <144124225+kohei-kusuyama@users.noreply.github.com>
2023-10-16 10:38:50 +0000

    [save]バリデーション

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,16 +1,24 @@
 class BooksController < ApplicationController
-  def new
-    @book = Book.new
-  end
+  # def new
+  #   @book = Book.new
+  # end
 
   def create
-    book = Book.new(book_params)
-    book.save
-    redirect_to '/books'
+    @book = Book.new(book_params)
+    if @book.save
+    flash[:notice] = "Book was successfully created."
+    redirect_to book_path(@book.id)
+    else
+    @books = Book.all
+    flash.now[:notice] = "投稿に失敗しました。"
+    render :index
+
+    end
   end
 
   def index
     @books = Book.all
+    @book = Book.new
   end
 
   def show
@@ -26,7 +34,7 @@ class BooksController < ApplicationController
     book.update(book_params)
     redirect_to book_path(book.id)
   end
-  
+
   def destroy
     book = Book.find(params[:id])
     book.destroy
